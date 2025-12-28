b8db47377c204d31782f8459000f26633fbd2649
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-28 14:41:25 +0300

    update

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -57,9 +57,10 @@ class StockPicking(models.Model):
     def _assign_ou_to_moves(self):
         for picking in self:
             if picking.operation_unit_id:
-                picking.move_lines.write({
-                    'operation_unit_id': picking.operation_unit_id.id
-                })
+                for line in picking.move_ids_without_package
+                    line.write({
+                            'operation_unit_id': picking.operation_unit_id.id
+                        })
 
  
 class StockMove(models.Model):
