fa8a2558ed7d63d45d5053a37f77b29058deea52
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-29 16:20:29 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -70,8 +70,8 @@ class AccountMove(models.Model):
   
     def _compute_payments_widget_to_reconcile_info(self):
         self.ensure_one()
-        result = super()._compute_payments_widget_to_reconcile_info()
-        print("_get_outstanding_info_JSON==============")
+        result = super(AccountMove,self)._compute_payments_widget_to_reconcile_info()
+        print("_get_outstanding_info_JSON==============",result)
         # لو ما فيه OU على الفاتورة → نرجع الطبيعي
         if not self.operation_unit_id or not result:
             return result
