c0fa8a8f7dd3241c8afb7ad6364ba242d225a95b
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-30 12:50:28 +0300

    udpte

--- models/account_payment.py
+++ models/account_payment.py
@@ -8,7 +8,7 @@ class AccountPayment(models.Model):
 
     operation_unit_id = fields.Many2one(
         'operation.unit',
-        readonly=True,
+     
         copy=False
     )
 
@@ -16,29 +16,29 @@ class AccountPayment(models.Model):
     def create(self, vals):
         print("vals.get('operation_unit_id'):*******************",vals.get('operation_unit_id'):)
 
-        if not vals.get('operation_unit_id'):
+        # if not vals.get('operation_unit_id'):
 
-            print("vals.get('operation_unit_id'):*******************",vals.get('operation_unit_id'):)
-            vals['operation_unit_id'] = self.env.user.default_ou_id.id
+        #     print("vals.get('operation_unit_id'):*******************",vals.get('operation_unit_id'):)
+        #     vals['operation_unit_id'] = self.env.user.default_ou_id.id
 
-        return super().create(vals)
+        # return super().create(vals)
 
 
-        # payment = super().create(vals)
+        payment = super().create(vals)
  
-        # if payment.move_id and payment.move_id.operation_unit_id:
-        #     payment.operation_unit_id = payment.move_id.operation_unit_id.id
+        if payment.move_id and payment.move_id.operation_unit_id:
+            payment.operation_unit_id = payment.move_id.operation_unit_id.id
 
   
-        # else:
-        #     payment.operation_unit_id = self.env.user.default_ou_id.id
+        else:
+            payment.operation_unit_id = self.env.user.default_ou_id.id
 
-        # return payment
+        return payment
    
-    @api.constrains('operation_unit_id')
-    def _check_ou_required(self):
-        for rec in self:
-            if not rec.operation_unit_id:
-                raise ValidationError(
-                    'Operation Unit is required'
-                )
+    # @api.constrains('operation_unit_id')
+    # def _check_ou_required(self):
+    #     for rec in self:
+    #         if not rec.operation_unit_id:
+    #             raise ValidationError(
+    #                 'Operation Unit is required'
+    #             )
