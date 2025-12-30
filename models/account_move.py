1f4b3361b3177430de6e343ad480fe9736d2a80a
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-30 12:03:09 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -220,6 +220,7 @@ class AccountMoveLine(models.Model):
     
     def reconcile(self):
         ous = self.mapped('operation_unit_id').filtered(lambda x: x)
+        print("ous********************************",ous)
         if len(ous) > 1:
             raise ValidationError(
                 'You cannot reconcile entries from different Operation Units.'
