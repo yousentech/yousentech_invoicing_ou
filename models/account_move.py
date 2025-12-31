7a2ddb676614e1a5d94e06d202447e533dde7279
yousentech <83219678+yousentech@users.noreply.github.com>
2025-12-31 17:32:16 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -83,7 +83,7 @@ class AccountMove(models.Model):
                     "The selected Operation Unit does not belong to the same company as this document."
                 ) 
 
-            if user.ou_config_ids.filtered(lambda x: x.company_id.id == picking.company_id.id).allowed_ou_ids and ou not in user.ou_config_ids.filtered(lambda x: x.company_id.id == picking.company_id.id).allowed_ou_ids:
+            if user.ou_config_ids.filtered(lambda x: x.company_id.id == move.company_id.id).allowed_ou_ids and ou not in user.ou_config_ids.filtered(lambda x: x.company_id.id == move.company_id.id).allowed_ou_ids:
 
                 raise ValidationError(
                     "The selected Operation Unit is not allowed for the current user."
