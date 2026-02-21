2ac8015be489578035b94aaff80c0d50c758e964
yousentech <83219678+yousentech@users.noreply.github.com>
2026-02-21 14:47:46 +0300

    updae

--- models/account_move.py
+++ models/account_move.py
@@ -83,6 +83,7 @@ class AccountMove(models.Model):
     def create(self, vals):
        
         if not vals.get('operation_unit_id'):
+            print('*****************************************')
             vals['operation_unit_id'] =  self.env.user.ou_config_ids.filtered(lambda x: x.company_id.id == vals.get('company_id')).default_ou_id.id
 
         return super().create(vals)
